dad501e195a7f1373dd3a9a62fc13e6fe0f844a9
unknown <angeloagoduto@lewisu.edu>
2021-11-05 11:53:04 -0500

    update file.

--- TurtleDraw-aag.py
+++ TurtleDraw-aag.py
@@ -10,8 +10,8 @@ import math
 
 TEXTFILENAME = 'turtle-draw.txt'
 
-turtleScreen = turtle.Screen()
-turtleScreen.setup(450, 450)
+turtleBoarder = turtle.Screen()
+turtleBoarder.setup(450, 450)
 
 # Todo: Ask user for the file name.
 
@@ -25,6 +25,14 @@ turtleDraw.penup()
 print('Reading a text file line by line.')
 turtleDrawTextfile = open(TEXTFILENAME, 'r')
 line = turtleDrawTextfile.readline()
+
+totalDistance = 0
+previousPoint = [0, 0]
+# currentPoint = [1, 1]
+# print (math.dist(previousPoint, currentPoint))
+# input("\nPress 'enter' to close Application...")
+
+
 while line:
     print(line, end='')
     parts = line.split(' ')
@@ -34,19 +42,28 @@ while line:
         x = int(parts[1])
         y = int(parts[2])
 
+        currentPoint = [x, y]
+        totalDistance = totalDistance + math.dist(previousPoint,currentPoint)
+        print(math.dist(previousPoint, currentPoint))
+        print(previousPoint)
+        print(currentPoint)
+        print('Total Distance=' + str(totalDistance))
+
+        # Todo: Find total distance when Pen is Down. Only when pen is down, add distance to total
+        
+        previousPoint = currentPoint
+
         turtleDraw.color(color)
         turtleDraw.goto(x,y)
 
         turtleDraw.pendown()
 
+# Todo: Total Distance Outdside of loop
+
     if (len(parts) == 1):
         turtleDraw.penup()
 
     line = turtleDrawTextfile.readline()
-
-# I think I'm right there for this problem, I just can't seem to figure out what to put so it grabs from the text file.
-    # turtleDistance = math.dist(x, y)
-    # print(turtleDistance)
 
 # Todo: Print the total near the bottom
 turtle.done()
